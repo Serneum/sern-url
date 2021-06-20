17d7e81cef3e10599bad03a77983b79c49eda15d
Chris Rees <crees@covermymeds.com>
2021-06-20 00:29:12 -0400

    Fix PageController test

--- test/sern_url_web/controllers/page_controller_test.exs
+++ test/sern_url_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SernUrlWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Shorten a URL"
   end
 end
